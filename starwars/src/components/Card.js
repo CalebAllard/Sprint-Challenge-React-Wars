27367ba446b7d4acc2542fc92b76c2b7e514374b
CalebAllard <allard.caleb@gmail.com>
2019-11-08 13:52:09 -0500

    finished mvp

--- starwars/src/components/Card.js
+++ starwars/src/components/Card.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import style from 'styled-components';
 
-const card = styled.div`
-
+const StyledCard = style.div`
+    background: white;
+    box-shadow: 0 1rem 1.5rem rgba(000,000,000,0.8);
+    padding: 1rem 1.5rem;
+    width:25%;
+    margin-bottom:1.5rem;
+    h3{
+        text-transform:uppercase;
+        font-size:1.8rem;
+    }
 
 `;
 
@@ -11,12 +19,12 @@ export default function Card(props){
 
     return(
 
-        <div className="card">
+        <StyledCard>
             <h3>{props.name}</h3>
-            <p>Birth Year:{props.birth_year}</p>
-            <p>Hair Color:{props.hair_color}</p>
+            <p>Birth Year: {props.birth_year}</p>
+            <p>Hair Color: {props.hair_color}</p>
 
-        </div>
+        </StyledCard>
 
 
     )
--- starwars/src/components/CardGroup.js
+++ starwars/src/components/CardGroup.js
@@ -1,50 +1,63 @@
 import React,{useState,useEffect} from 'react';
 import axios from 'axios';
 import Card from './Card';
+import styled from 'styled-components';
 
+
+const CardsGroup = styled.div`
+    display:flex;
+    flex-wrap:wrap;
+    justify-content: space-around;
+    align-content:space-around;
+`;
 
 export default function CardGroup(){
     
+
+
     
-    const [people, setPeople] = useState({});
-    let tempArray = {};
+    const [people, setPeople] = useState([]);
+    let tempArray = [];
      useEffect(() => {
         axios.get('https://cors-anywhere.herokuapp.com/https://lambda-swapi.herokuapp.com/api/people/')
         .then(response => {
-            console.log(response);
-            setPeople(response.data.results);
-            console.log(people);
+            // console.log(response);
+             return setPeople(response.data.results);
+            // console.log(people);
         // for (let key in people.results){
         //     if (people.results.hasOwnProperty(key)){
         //         tempArray.push(people.results[key]);
         //     } 
         // }
-         
+        // return tempArray; 
             
         
          })
          .catch(error =>{console.log(`error: ${error}`)});
      },[]);
-        // let tempArray = {}
-        // for (let key in people){
-        //     if (people.hasOwnProperty(key)){
-        //         tempArray.push(people[key]);
-        //     }
-        // }
         // console.log(tempArray);
+        // let tempArray = {}
+        for (let key in people){
+            if (people.hasOwnProperty(key)){
+                tempArray.push(people[key]);
+            }
+        }
+        console.log(tempArray);
 
     return (
         
-        <div className='CardGroup'>
+        <CardsGroup>
             {
-
+                tempArray.map((item,key) =>
+                    <Card key={key} name={item.name} birth_year={item.birth_year} hair_color={item.hair_color}/>                
+                )
             }
 
            
                
-            <Card />
+            {/* <Card /> */}
 
-        </div>
+        </CardsGroup>
         
     )
 
